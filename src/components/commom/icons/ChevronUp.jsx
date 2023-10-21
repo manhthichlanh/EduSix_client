09c503e3ff458081d8123ccf1a5900936f959b55
thuydan0801 <ngothuydan2003@gmail.com>
2023-10-21 21:03:45 +0700

    ngothuyyeah create cpn icons

---
+++ src/components/commom/icons/ChevronUp.jsx
@@ -0,0 +1,8 @@
+
+export default function ChecvronUp(props) {
+    return (
+        <svg {...props} className="" width="" height="" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15 12.5L10 7.5L5 12.5" stroke="#333333" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,18 @@
 import { io } from "socket.io-client";
 import TableWiget from "../../components/OrderBookTable/TableWiget";
 import { useState } from 'react';
+import Star from "../../components/commom/icons/Star"
+import StarHalf from "../../components/commom/icons/StarHalf"
+import StarFill from "../../components/commom/icons/StarFill"
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Star></Star>
+      <StarHalf></StarHalf>
+      <StarFill></StarFill>
     </>
   )
 }
