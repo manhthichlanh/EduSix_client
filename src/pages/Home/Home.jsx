d0060544e3560922c74e644de37abcab450011d2
thuydan0801 <ngothuydan2003@gmail.com>
2023-10-22 09:37:07 +0700

    test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,15 +5,13 @@ import { useState } from 'react';
 import Star from "../../components/commom/icons/Star"
 import StarHalf from "../../components/commom/icons/StarHalf"
 import StarFill from "../../components/commom/icons/StarFill"
+import Search from "../../components/commom/icons/Search"
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <Star></Star>
-      <StarHalf></StarHalf>
-      <StarFill></StarFill>
     </>
   )
 }
