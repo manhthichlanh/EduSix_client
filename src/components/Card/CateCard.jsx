96879be0f1d31a865a86b73deae5c51876faf74d
= <=pdat9776@gmail.com>
2023-12-22 02:57:17 +0700

    Đạt

--- src/components/Card/CateCard.jsx
+++ src/components/Card/CateCard.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
-
+import { serverEndpoint } from "../../utils/http";
 // import { serverEndpoint } from "../../utils/http";
 export default function CateCard(props) {
   // eslint-disable-next-line react/prop-types
   const { image, cateName } = props;
+
+  // Kiểm tra nếu image bắt đầu bằng 'http'
+  const imageUrl = image.startsWith('http') ? image : `${serverEndpoint}category/logo_cate/${image}`;
+
   return (
     <div className="my-4">
       <div className="w-full py-10 shadow-md rounded-xl">
         <Link to="/">
           <img
             className="pb-6 mx-auto"
-            src={`${`http://14.225.198.206:8080/`}category/logo_cate/${image}`}
+            src={imageUrl}
             alt=""
           />
         </Link>
